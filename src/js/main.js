c4e9c6c23065cb490649b49d44e257c5ef1bb76c
Nick <gvmbrtell@gmail.com>
2018-09-22 14:37:44 -0600

    Build faq

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(() => {
 
+  // about
   $('#about-cta').click(() => {
     $('#about-more').toggle();
   });
 
+  // faq - why
+  $('#whyburger-cta').click(() => {
+    $('#whyburger-more').toggle();
+  });
+
+  // faq - where
+  $('#wherebeef-cta').click(() => {
+    $('#wherebeef-more').toggle();
+  });
+  
 });
