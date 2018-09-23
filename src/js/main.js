6e762d5f9548328caba3d862e0b11b14deebdf4e
Nick <gvmbrtell@gmail.com>
2018-09-22 22:21:32 -0600

    Add form modals

--- src/js/main.js
+++ src/js/main.js
@@ -14,5 +14,30 @@ $(document).ready(() => {
   $('#wherebeef-cta').click(() => {
     $('#wherebeef-more').toggle();
   });
+
+
+  $('#sub-open').click(() => {
+    $('#sub').css('display', 'flex');
+    window.scrollTo(0, 0);
+    $('body').css('overflow', 'hidden');
+  });
+  $('#sub-close').click(() => {
+    $('#sub').css('display', 'none');
+    window.scrollTo(0, 10000);
+    $('body').css('overflow', 'auto');
+  });
+
+  $('#work-open').click(() => {
+    $('#work').css('display', 'flex');
+    window.scrollTo(0, 0);
+    $('body').css('overflow', 'hidden');
+  });
+  $('#work-close').click(() => {
+    $('#work').css('display', 'none');
+    window.scrollTo(0, 10000);
+    $('body').css('overflow', 'auto');
+  });
+  
+  
   
 });
